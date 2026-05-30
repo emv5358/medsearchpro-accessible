574d25687198b33809f960c05f3eb56b599aa8c1
Ehsaneh (Emma) Vilataj <emv5358@psu.edu>
2026-05-29 23:47:48 -0700

    Initial website deployment

---
+++ README.txt
@@ -0,0 +1,4 @@
+
+cd "/Users/test/Desktop/Ehsaneh/Business_Plan/medsearchpro-accessible"
+
+/Users/test/.nvm/versions/node/v20.19.6/bin/node ./node_modules/vite/bin/vite.js --host 0.0.0.0
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+      <main>
+        <h1>MedSearchPro is working</h1>
+        <p>If you see this, React is connected correctly.</p>
+      </main>
+    );
+  }
